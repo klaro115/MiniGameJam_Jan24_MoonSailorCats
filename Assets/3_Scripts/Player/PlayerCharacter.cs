f57d8b91c8b54a7f7680f6cfc92aa0e1c6d34787
Robert Klares <klaro115@hotmail.com>
2024-01-20 15:06:57 +0100

    Player can move with WASD or arrow keys

--- Assets/3_Scripts/Player/PlayerCharacter.cs
+++ Assets/3_Scripts/Player/PlayerCharacter.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Player
@@ -8,17 +6,39 @@ namespace Player
     {
         #region Fields
 
-        #endregion
-        #region Methods
+        public PlayerInputType inputType = PlayerInputType.WASD;
+		public float walkSpeed = 3.0f;
 
-        void Start()
-        {
-        
-        }
+        private CharacterController ctrl = null;
 
-        void Update()
+		#endregion
+		#region Methods
+
+		private void Start()
+		{
+            ctrl = GetComponent<CharacterController>();
+		}
+
+		void Update()
         {
-        
+            Vector2 inputAxis;
+            switch (inputType)
+            {
+                case PlayerInputType.WASD:
+                    inputAxis = new Vector2(Input.GetAxisRaw("Horizontal_AD"), Input.GetAxisRaw("Vertical_WS"));
+					break;
+                case PlayerInputType.ArrowKeys:
+                    inputAxis = new Vector2(Input.GetAxisRaw("Horizontal_LeftRight"), Input.GetAxisRaw("Vertical_UpDown"));
+                    break;
+				default:
+                    inputAxis = Vector2.zero;
+					break;
+            }
+
+            Vector3 moveDir = new Vector3(inputAxis.x, inputAxis.y, 0).normalized;
+            Vector3 moveSpeed = Time.deltaTime * walkSpeed * moveDir;
+
+            ctrl.Move(moveSpeed);
         }
 
         #endregion
